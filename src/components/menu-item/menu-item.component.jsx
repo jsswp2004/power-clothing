5424ad421e4204e0e8333f0e44dc4fb4449687d1
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2021-12-22 16:55:21 -0500

    ss

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-import "./menu-item.styles.scss";
+import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl }) => (
-    <div style={{ backgroundImage: `url(${imageUrl})` }}
+    <div style={{
+        backgroundImage: `url(${imageUrl})`
+    }}
         className="menu-item">
     <div className="content">
       <h1 className="title">{title}</h1>
